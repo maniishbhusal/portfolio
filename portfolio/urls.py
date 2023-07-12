85c5a272d499c38972532ffe2548503410117f24
Manish Bhusal <manishbhusal6@gmail.com>
2023-07-12 15:10:52 +0545

    Complete static About, Portfolio and Blogs page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('blogs/', views.blogs_page, name='blogs_page'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request,'portfolio/home.html')
+
+def about(request):
+    return render(request,'portfolio/about.html')
+
+def portfolio(request):
+    return render(request,'portfolio/portfolio.html')
+
+def blogs_page(request):
+    return render(request,'portfolio/blogs_page.html')
