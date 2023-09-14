35502e0cf8918605c9bbbabc48d026612843d135
Manish Bhusal <manishbhusal6@gmail.com>
2023-09-14 14:36:56 +0545

    done vercel settings

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 # build_files.sh
 
-pip install -r requirements.txt
+echo " BUILD START"
+python3.11 -m pip install -r requirements.txt
 python3.11 manage.py collectstatic --no-input --clear
+echo " BUILD END"
